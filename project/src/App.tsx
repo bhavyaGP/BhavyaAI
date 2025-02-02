be886d0ceddbbf26fae86d8f38c3ddb48f663524
Bhavya Prajapati <126806988+bhavyaGP@users.noreply.github.com>
2025-02-02 21:22:52 +0530

    Update App.tsx

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -42,7 +42,7 @@ function App() {
       const config = {
         method: 'post',
         maxBodyLength: Infinity,
-        URL: import.meta.env.VITE_BACKEND_URL,
+        URL: "https://bhavya-ai-uz8s.vercel.app/ask",
         headers: {
           'Content-Type': 'application/json',
         },
